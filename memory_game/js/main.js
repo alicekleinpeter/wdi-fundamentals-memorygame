05a85c048c4c4670d2551f4fb9bcafa78325f3d1
Alice Kleinpeter <34873579+alicekleinpeter@users.noreply.github.com>
2018-01-09 14:54:54 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,21 @@
-console.log("Up and running!");
-console.log("User flipped " + cardOne);
-//console.log("User flipped " + cardThree);
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+cardsInPlay.push('cards[cardId]');
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+		alert("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+		alert("Sorry, try again!");
+	}
+}
+var flipCard = function(cardId) {
+console.log("User flipped " + cards[cardId]);
+checkForMatch();
+}
 
-if (cardsInPlay[0] === cardsInPlay[2]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again!");
-};
+flipCard(0);
+flipCard(2);
+
