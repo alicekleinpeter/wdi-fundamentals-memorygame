97f56a52784b36834d6cbc762a3f3f06dd4e8a28
Alice Kleinpeter <34873579+alicekleinpeter@users.noreply.github.com>
2018-01-10 09:12:44 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen" ,
+suit: "Hearts" ,
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen" ,
+suit: "diamonds" ,
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king" ,
+suit: "hearts" ,
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king" ,
+suit: "diamonds" ,
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
-cardsInPlay.push('cards[cardId]');
+cardsInPlay.push('cards[cardId].rank');
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
@@ -12,10 +33,11 @@ var checkForMatch = function() {
 	}
 }
 var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
 checkForMatch();
+console.log(cards[cardId].cardImage);
+//console.log(cards[cardId].suit);
 }
-
 flipCard(0);
 flipCard(2);
 
